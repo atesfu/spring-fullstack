ef0ebe8bf72ca446971b644fc63d005d3ae8fafb
Aron <atesfu@miu.edu>
2022-06-01 16:52:31 -0500

    clone stage

---
+++ src/main/java/com/example/demo/student/StudentService.java
@@ -0,0 +1,24 @@
+package com.example.demo.student;
+
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@AllArgsConstructor
+@Service
+public class StudentService {
+    private final StudentRepository studentRepository;
+
+    public List<Student> getAllStudents(){
+        return studentRepository.findAll();
+    }
+
+    public void addStudent(Student student) {
+        studentRepository.save(student);
+    }
+
+    public void deleteStudent(Long studentId) {
+        studentRepository.deleteById(studentId);
+    }
+}
